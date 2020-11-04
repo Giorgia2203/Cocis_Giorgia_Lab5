849336207a5355586b23096d94241226c1d58c1e
giorgia220523 <cocisgiorgia@gmail.com>
2020-11-04 20:24:32 +0200

    Laborator 5-04.11.2020

---
+++ Model/ExpenseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cocis_Giorgia_Lab5.Model
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Cocis_Giorgia_Lab5.Model;
 
 namespace Cocis_Giorgia_Lab5
 {
@@ -25,6 +27,7 @@ namespace Cocis_Giorgia_Lab5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ExpenseContext>(opt => opt.UseInMemoryDatabase("ExpenseList"));
             services.AddControllers();
         }
 
