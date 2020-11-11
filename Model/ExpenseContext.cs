cc64cdf1430adb59db7496e1ed88e315758b1c3d
giorgia220523 <cocisgiorgia@gmail.com>
2020-11-11 19:53:34 +0200

    Laborator 6-11.11.2020

--- Model/ExpenseContext.cs
+++ Model/ExpenseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Cocis_Giorgia_Lab5.Model;
 
 namespace Cocis_Giorgia_Lab5.Model
 {
@@ -14,5 +15,7 @@ namespace Cocis_Giorgia_Lab5.Model
         }
 
         public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+        public DbSet<Budget> Budget { get; set; }
     }
 }
